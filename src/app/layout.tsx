fbc2f4fef27649d7e1a54b7b97ed23ddbb111189
Facundo Rubino <facundorubino21@gmail.com>
2025-07-14 21:05:44 -0300

    feat: add WhatsApp button component and integrate it into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import WhatsAppButton from '../components/ui/wppButton';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={cn(inter.className, "antialiased")}>
+        <WhatsAppButton />
         {children}
       </body>
     </html>
